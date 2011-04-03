2b41229fba302d1caa905a40729047255e1847a4
Hiromi Ishii <konn.jinro_at_gmail.com>
2011-04-03 13:55:00 +0900

    mew

--- Utils.hs
+++ Utils.hs
@@ -1,1 +1,9 @@
-module Utils () where
+{-# LANGUAGE CPP #-}
+module Utils ((=$)) where
+import Data.Enumerator
+
+#ifdef OLD_ENUMERATOR
+(=$) :: Monad m => Enumeratee ao ai m b -> Iteratee ai m b -> Iteratee ao m b
+(=$) = (joinI .) . ($$)
+infixr 0 =$
+#endif
