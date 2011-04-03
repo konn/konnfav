e66110703d52b6ec79b45fb34741e29b309512af
Hiromi Ishii <konn.jinro_at_gmail.com>
2011-04-03 13:49:13 +0900

    removed protected user

--- Handler/Root.hs
+++ Handler/Root.hs
@@ -8,6 +8,7 @@ import Data.Enumerator hiding (mapM, consume)
 import qualified Data.Enumerator as E
 import Data.Enumerator.List hiding (mapM)
 import qualified Data.Enumerator.List as EL
+import Control.Monad
 
 -- This is a handler function for the GET request method on the RootR
 -- resource pattern. All of your resource patterns are defined in
@@ -20,9 +21,17 @@ getRootR :: Handler RepHtml
 getRootR = do
     tws <- runDB $ do
       let tweets = select [] [TweetCreatedAtDesc] 0 0
-      run_ (tweets $$ EL.mapM (favWithUsers.snd) =$ EL.filter (not . null . snd) =$ isolate 20 =$ consume)
+      run_ $ tweets $$ EL.mapM (favWithUsers.snd) =$ EL.filter (not . null . snd) =$
+               EL.filterM (isProtectedTweet.fst) =$ isolate 20 =$ consume
     tweets <- mapM renderTweet tws
     defaultLayout $ do
         h2id <- lift newIdent
         setTitle "konnfav homepage"
         addWidget $(widgetFile "homepage")
+
+isProtectedTweet :: (PersistBackend m, Functor m) => Tweet -> m Bool
+isProtectedTweet tw = do
+  musr <- liftM (userProtected . snd) <$> getBy (UserScreenName $ tweetUser tw)
+  case musr of
+    Nothing -> return False
+    Just pr -> return pr
--- Utils.hs
+++ Utils.hs
@@ -1,7 +1,1 @@
-module Utils ((=$)) where
-import Data.Enumerator
-
-(=$) :: Monad m => Enumeratee ao ai m b -> Iteratee ai m b -> Iteratee ao m b
-(=$) = (joinI .) . ($$)
-
-infixr 0 =$
+module Utils () where
